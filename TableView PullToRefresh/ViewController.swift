3831eb9735211246702d04823dc7f4079cde8422
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-16 23:08:18 +0530

    Pull to refresh on tableView

--- TableView PullToRefresh/ViewController.swift
+++ TableView PullToRefresh/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+//    lazy var refresher: UIRefreshControl = {
+//        let refreshControl = UIRefreshControl()
+//        refreshControl.tintColor = .white
+//        refreshControl.addTarget(self, action: #selector(requestData), for: .valueChanged)
+//        return refreshControl
+//    }()
+    
+    var refresher = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        self.navigationController?.navigationBar.prefersLargeTitles = true
+//        tableView.tableFooterView = UIView()
+        
+        refresher.tintColor = .white
+        refresher.addTarget(self, action: #selector(requestData), for: .valueChanged)
+        
+        if #available(iOS 10.0, *){
+            tableView.refreshControl = refresher
+        }
+        else{
+            tableView.addSubview(refresher)
+        }
+    }
+    
+    @objc
+    func requestData(refreshControl: UIRefreshControl) {
+        print("refreshing")
+        let deadline = DispatchTime.now() + .milliseconds(1000)
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
+            refreshControl.endRefreshing()
+            print("fetched data")
+        }
     }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
 }
 
